a460cc266b42b56690584ec482bd2b414ec554a3
filipe.leite <filipedasilva94.1@gmail.com>
2023-07-11 23:26:09 -0300

    Aula 9 terminada

--- modulo 1/script.js
+++ modulo 1/script.js
@@ -4,3 +4,24 @@
 var hello = "hello world!!"
 
 */
+
+function entrar(){
+
+    var area = document.getElementById('area');
+    var texto = prompt('Digite seu nome?');
+
+    if(texto =='' || texto==null){
+        alert('Digite seu nome novamente!');
+        area.innerHTML = 'Bem vindo...';
+    } 
+    else {
+        area.innerHTML = 'Bem vindo ' + texto;
+    }
+}
+
+
+function entrar2(nome){
+    var area = document.getElementById('area2');
+    var texto = prompt('Digite seu sobrenome?');
+    area.innerHTML = nome + " " + texto;
+}
