1242810b5319841e44e4c84805955fdccf0faed4
filipe.leite <filipedasilva94.1@gmail.com>
2023-07-17 01:05:17 -0300

    Lesson 40 finished

--- modulo_5/meusite/src/App.js
+++ modulo_5/meusite/src/App.js
@@ -1,34 +1,93 @@
 import React, {Component} from 'react';
+//import Membro from './components/membro';
+
+//  Aula 37
+//
+// class App extends Component{
+//
+//   constructor(props){
+//     super(props);
+//     this.state = {
+//       hora: '00:00:00'
+//     };
+//   }
+//
+//   componentDidMount(){
+//
+//     setInterval(() => {
+//       this.setState({ hora: new Date().toLocaleTimeString() })
+//       },1000);
+//
+//   }
+//
+//   // componentDidUpdate(){
+//   //   console.log('Atualizou!!!');
+//   // }
+//
+//   render(){
+//     return(
+//       <div>
+//         <h1>Meu Projeto {this.state.hora}</h1>
+//       </div>
+//     );
+//   }
+//
+//   }
+
+// 
+
+// // Aula 39
+
+// class  App extends Component{
+
+//   render(){
+//     return(
+//       <div>
+//         <Membro nome="Visitante"/>
+//       </div>
+//     )
+//   }
+
+// }
 
 class App extends Component{
 
   constructor(props){
-    super(props);
-    this.state = {
-      hora: '00:00:00'
-    };
+      super(props);
+      this.state = {
+          status: false
+      };
+
+      this.entrar = this.entrar.bind(this);
+      this.sair = this.sair.bind(this);
   }
 
-  componentDidMount(){
-
-    setInterval(() => {
-      this.setState({ hora: new Date().toLocaleTimeString() })
-      },1000);
-
+  entrar(){
+    this.setState({status: true});
   }
 
-  // componentDidUpdate(){
-  //   console.log('Atualizou!!!');
-  // }
+  sair(){
+    this.setState({status: false});
+  }
 
   render(){
-    return(
-      <div>
-        <h1>Meu Projeto {this.state.hora}</h1>
-      </div>
-    );
+      return(
+          <div>
+              {
+                  this.state.status ? 
+                  <div>
+                    <h2>Bem-vindo ao sistema</h2>
+                    <button onClick={this.sair}>Entrar no sistema</button>
+                  </div> :
+                  <div>
+                    <h2>Olá, visitante. Faça o login!</h2>
+                    <button onClick={this.entrar}>Sair do sistema</button>
+                  </div>
+              }
+          </div>
+      );
   }
 
-  }
+}
 
 export default App;
