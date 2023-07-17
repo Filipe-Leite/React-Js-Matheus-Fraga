9ea33d6d9c880f790cdea254c40d81dc68da6e17
filipe.leite <filipedasilva94.1@gmail.com>
2023-07-17 04:20:02 -0300

    Lesson 41 finished

--- modulo_5/meusite/src/App.js
+++ modulo_5/meusite/src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 //import Membro from './components/membro';
+import Feed from './components/feed';
 
 //  Aula 37
 //
@@ -50,40 +51,78 @@ import React, {Component} from 'react';
 
 // }
 
+// // <Aula 40>
+
+// class App extends Component{
+
+//   constructor(props){
+//       super(props);
+//       this.state = {
+//           status: false
+//       };
+
+//       this.entrar = this.entrar.bind(this);
+//       this.sair = this.sair.bind(this);
+//   }
+
+//   entrar(){
+//     this.setState({status: true});
+//   }
+
+//   sair(){
+//     this.setState({status: false});
+//   }
+
+//   render(){
+//       return(
+//           <div>
+//               {
+//                   this.state.status ? 
+//                   <div>
+//                     <h2>Bem-vindo ao sistema</h2>
+//                     <button onClick={this.sair}>Entrar no sistema</button>
+//                   </div> :
+//                   <div>
+//                     <h2>Olá, visitante. Faça o login!</h2>
+//                     <button onClick={this.entrar}>Sair do sistema</button>
+//                   </div>
+//               }
+//           </div>
+//       );
+//   }
+
+// }
+
+// export default App;
+
+// ////////////// <Aula 40/>
+
 class App extends Component{
 
   constructor(props){
       super(props);
       this.state = {
-          status: false
+          feed:[
+            {id: 1, username: 'Matheus', curtidas:10, comentarios:2},
+            {id: 2, username: 'Lucas', curtidas: 120, comentarios:24},
+            {id: 3, username: 'Amanda', curtidas: 30, comentarios:12},
+            {id: 4, username: 'Filipe', curtidas: 1, comentarios:1}
+          ]
       };
 
-      this.entrar = this.entrar.bind(this);
-      this.sair = this.sair.bind(this);
-  }
-
-  entrar(){
-    this.setState({status: true});
-  }
-
-  sair(){
-    this.setState({status: false});
   }
 
   render(){
       return(
           <div>
-              {
-                  this.state.status ? 
-                  <div>
-                    <h2>Bem-vindo ao sistema</h2>
-                    <button onClick={this.sair}>Entrar no sistema</button>
-                  </div> :
-                  <div>
-                    <h2>Olá, visitante. Faça o login!</h2>
-                    <button onClick={this.entrar}>Sair do sistema</button>
-                  </div>
-              }
+            {this.state.feed.map((item)=>{
+              return(
+                <Feed id={item.id} 
+                      username={item.username}
+                      curtidas={item.curtidas}
+                      comentarios={item.comentarios}/>
+              );
+            })}
           </div>
       );
   }
