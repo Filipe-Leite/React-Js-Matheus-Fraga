a994712c0096668d747a6a53d1684855b8d9e4ab
filipe.leite <filipedasilva94.1@gmail.com>
2023-07-23 14:34:19 -0300

    Lesson 64 Finished

--- modulo_8/prime/src/pages/Filme/index.js
+++ modulo_8/prime/src/pages/Filme/index.js
@@ -1,7 +1,63 @@
+import { useEffect,useState } from "react";
+import {useParams} from 'react-router-dom';
+import './filme-info.css';
+
+import api from '../../services/api';
+
 function Filme(){
+    const { id } = useParams();
+    const [filme, setFilme] = useState({}); 
+    const [loading, setLoading] = useState(true);
+
+    useEffect(()=>{
+        async function loadFilme(){
+            await api.get(`/movie/${id}`,{
+                params:{
+                    api_key: "ad4b1212027d289eb922748d6e4fe544",
+                    language: "pt-BR"
+                }
+            })
+            .then((response)=>{
+                setFilme(response.data);
+                setLoading(false);
+            })
+            .catch(()=>{
+                console.log("Filme nao encontrado")
+            })
+        }
+
+        loadFilme();
+
+        return () => {
+            console.log("COMPONENTE FOI DESMONTADO")
+        }
+    }, [])
+
+    if(loading){
+        return(
+            <div className="filme-info">
+                <h1>Carregando detalhes...</h1>
+            </div>
+        )
+    }
+
     return(
-        <div>
-            <h1>BEM VINDO A Filme</h1>
+        <div className="filme-info">
+            <h1>{filme.title}</h1>
+            <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
+
+            <h3>Sinopse</h3>
+            <span>{filme.overview}</span>
+
+            <strong>Avaliação: {filme.vote_average} / 10</strong>
+        
+            <div className="area-buttons">
+                <button>Salvar</button>
+                <button>
+                    <a href="#"></a>
+                    Trailer
+                </button>
+            </div>
         </div>
     );
 }
--- modulo_8/prime/src/pages/Home/index.js
+++ modulo_8/prime/src/pages/Home/index.js
@@ -1,7 +1,53 @@
+import { useEffect,  useState } from 'react';
+import api from '../../services/api';
+import { Link } from 'react-router-dom';
+import './home.css';
+
 function Home(){
+    const [filmes, setFilmes] = useState([]);
+    const [loading,setLoading] = useState(true);
+
+    useEffect(()=>{
+
+        async function loadFilmes(){
+            const response = await api.get("movie/now_playing", {
+                params:{
+                    api_key: "ad4b1212027d289eb922748d6e4fe544",
+                    language: "pt-BR",
+                    page: 1
+                }
+            })
+
+            //console.log(response.data.results.slice(0,10));
+            setFilmes(response.data.results.slice(0,10))
+            setLoading(false);
+        }
+
+        loadFilmes();
+
+    },[])
+
+    if(loading){
+        return(
+            <div className="loading">
+                <h2>Carregando filmes...</h2>
+            </div>
+        )
+    }
+
     return(
-        <div>
-            <h1>BEM VINDO A Home</h1>
+        <div className='container'>
+            <div className='lista-filmes'>
+                {filmes.map((filme) => {
+                    return(
+                        <article key={filme.id}>
+                                <strong>{filme.title}</strong>
+                                <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title}></img>
+                                <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                        </article>
+                    )
+                })}
+            </div>
         </div>
     )
 }
